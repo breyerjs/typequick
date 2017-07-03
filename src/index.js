87fc4cd6d2c03a6968f6a2c9d04c2590a69c2a9f
breyerjs <breyer.js@gmail.com>
2017-07-03 14:23:24 -0700

    Force site to use https.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './css/App.css';
+import Config from './configuration/config.json';
 
+const currentUrl = window.location.href;
 
-ReactDOM.render(
-  <App className="app-root"/>,
-  document.getElementById('root')
-);
+// If we're on http, redirect to https
+if (! currentUrl.includes(Config.securePrefix) && ! currentUrl.includes(Config.localUrl)){
+  window.location.replace(Config.secureSiteUrl);
+} else{
+  ReactDOM.render(
+    <App className="app-root"/>,
+    document.getElementById('root')
+  );
+}
